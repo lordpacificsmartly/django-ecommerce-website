d729ec7ef16c20e26827bfa4d1eab3a2272d2e8a
Jesse <jesseonoyeyan@yahoo.com>
2022-01-22 13:03:08 +0100

    allowed host

--- rodbuds/rodbuds/settings.py
+++ rodbuds/rodbuds/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['rodbudsapp-env.eba-pcprdnzv.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['rodbuds-app-env.eba-pukbnh4i.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
